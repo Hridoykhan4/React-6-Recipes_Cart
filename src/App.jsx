990ac9939dd3eec3be9fbc340cce5273d73e586a
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-02-24 23:10:49 +0600

    search func added

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [items, setItems] = useState([]);
   const [spinner, setSpinner] = useState(false);
   const [orderItems, setOrderItems] = useState([]);
-
+  const [tempItem, setTempItems] = useState([]);
   useEffect(() => {
     const loadData = async () => {
       const res = await fetch("https://dummyjson.com/recipes");
@@ -22,6 +22,7 @@ function App() {
       setItems(data.recipes);
       setCartLoad(data.recipes);
       setSpinner(false);
+      setTempItems(data.recipes);
     };
     setSpinner(true);
     loadData();
@@ -67,17 +68,70 @@ function App() {
     }
   };
 
+  const handleInputSearch = (e) => {
+    const target = e.target.value;
+    if (target) {
+      const gatherMatched = [];
+      for (const e of tempItem) {
+        if (e.name.toLowerCase().includes(target.toLowerCase())) {
+          gatherMatched.push(e);
+        }
+      }
+      if (gatherMatched) {
+        setItems(gatherMatched);
+      } else {
+        setItems(tempItem);
+      }
+    }
+  };
+
   return (
     <>
-      <div className="navbar bg-primary text-primary-content">
-        <button className="btn btn-ghost text-xl">Bistro Restaurant</button>
+      <div className="navbar flex justify-between bg-primary text-primary-content">
+        <button className="btn btn-ghost sm:text-xl text-lg">
+          Bistro Restaurant
+        </button>
+        <div className="text-black">
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input
+              onChange={(e) => handleInputSearch(e)}
+              type="search"
+              className="text-lg"
+              required
+              placeholder="Search"
+            />
+          </label>
+        </div>
       </div>
+
+      <div className="w-11/12 mx-auto my-3">
+        <h3 className="text-lg font-semibold">
+          Our Available Food Items: {items.length}
+        </h3>
+      </div>
+
       {spinner && (
         <div className="flex justify-center my-4">
           <span className="loading loading-bars loading-xl"></span>
         </div>
       )}
-      <section className="max-w-[1440px] w-11/12 mx-auto flex gap-5 flex-col-reverse md:flex-row py-10">
+      <section className="max-w-[1440px] w-11/12 mx-auto flex gap-5 flex-col-reverse md:flex-row pb-10">
         <FoodItems
           handleAddToOrderItems={handleAddToOrderItems}
           items={items}
